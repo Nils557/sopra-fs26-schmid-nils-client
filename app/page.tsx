b00cad97708d1c1376a7fd44a9886298bfbd8aee
Nils <nilsojustdoesit@gmail.com>
2026-02-27 10:47:46 +0100

    Vixed Vercel bug (hopefully)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import { useRouter } from "next/navigation"; // use NextJS router for navigation
-import { Button, Form, Input } from "antd";
+import { Button, Form } from "antd";
 
 
 export default function Home() {
--- app/resetpassword/page.tsx
+++ app/resetpassword/page.tsx
@@ -1,102 +1,106 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client"; 
 
-import { useRouter } from "next/navigation"; // use NextJS router for navigation
+import React from "react";
+import { useRouter } from "next/navigation"; 
 import { useApi } from "@/hooks/useApi";
 import { Button, Form, Input, message, App } from "antd";
 
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
+interface ResetPasswordValues {
+  npassword: string;
+  repeat_password: string;
+}
 
-
-    const Login: React.FC = () => {
+const ResetPassword: React.FC = () => {
     const router = useRouter();
     const apiService = useApi();
     const [form] = Form.useForm();
 
-    const resetPassword = async (values: any) => {
-        const {npassword, repeat_password} = values;
+    const resetPassword = async (values: ResetPasswordValues) => {
+        const { npassword, repeat_password } = values;
         const userIdRaw = localStorage.getItem('userId');
         const userId = userIdRaw ? userIdRaw.replace(/"/g, '') : ''; 
-
-await apiService.put("/users/" + userId, {
-  password: npassword 
-});
 
         if (npassword !== repeat_password) {
             message.error("The passwords must match!");
             return;
         }
 
-        try {
+        if (!userId) {
+            message.error("User ID not found. Please log in again.");
+            return;
+        }
 
-            await apiService.put("/users/" + userId, {
-            password: npassword 
+        try {
+            await apiService.put(`/users/${userId}`, {
+                password: npassword 
             });
             
             message.success("Password changed successfully");
-            router.push("/login")
+            router.push("/login");
         } catch (error) {
+            console.error("Update failed:", error);
             message.error("Failed to update password");
         }
     };
-
 
     return (
         <App> 
         <div className="login-container">
             <Form
-            form={form}
-            name="login"
-            size="large"
-            variant="outlined"
-            onFinish={resetPassword}
-            layout="vertical"
+                form={form}
+                name="login"
+                size="large"
+                variant="outlined"
+                onFinish={resetPassword}
+                layout="vertical"
             >
-            <Form.Item>
-                <h1>Change your password</h1>
-                You are currently logged in to the account with the id: {localStorage.getItem('userId')}
-                <p>You are changing the password linked to your id.</p>
-            </Form.Item>
-            <Form.Item
-                name="npassword"
-                label="New password"
-                rules={[{ required: true, message: "Please input your new password!" }]}
-            >
-                <Input.Password placeholder="Enter new password" />
-            </Form.Item>
-            
-            <Form.Item
-                name="repeat_password"
-                label="Repeat new password"
-                dependencies={['npassword']} 
-                rules={[
-                { required: true, message: "Please repeat your password" },
-                ({ getFieldValue }) => ({
-                    validator(_, value) {
-                    if (!value || getFieldValue('npassword') === value) {
-                        return Promise.resolve();
-                    }
-                    return Promise.reject(new Error('The two passwords do not match!'));
-                    },
-                }),
-                ]}
-            >
-                <Input.Password placeholder="Enter password" />
-            </Form.Item>
+                <Form.Item>
+                    <h1>Change your password</h1>
+                    <p>You are changing the password linked to your ID.</p>
+                </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-button">
-                Reset
-                </Button>
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="button" className="grey-button" onClick={() => router.back()}>
-                Back
-                </Button>
-            </Form.Item>
+                <Form.Item
+                    name="npassword"
+                    label="New password"
+                    rules={[{ required: true, message: "Please input your new password!" }]}
+                >
+                    <Input.Password placeholder="Enter new password" />
+                </Form.Item>
+                
+                <Form.Item
+                    name="repeat_password"
+                    label="Repeat new password"
+                    dependencies={['npassword']} 
+                    rules={[
+                    { required: true, message: "Please repeat your password" },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                        if (!value || getFieldValue('npassword') === value) {
+                            return Promise.resolve();
+                        }
+                        return Promise.reject(new Error('The two passwords do not match!'));
+                        },
+                    }),
+                    ]}
+                >
+                    <Input.Password placeholder="Enter password" />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-button">
+                    Reset
+                    </Button>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="default" htmlType="button" className="grey-button" onClick={() => router.back()}>
+                    Back
+                    </Button>
+                </Form.Item>
             </Form>
         </div>
         </App> 
     );
-    };
-export default Login;
+};
+
+export default ResetPassword;
